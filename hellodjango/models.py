8e47ba27f18d0445cfb62cac6cb6e710366c2dc5
Daniel Fermanian <dfermani@dfermani-mn.linkedin.biz>
2012-08-04 00:02:34 -0700

    added more models

--- hellodjango/models.py
+++ hellodjango/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+class User(models.Model):
+	email = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
+
+class Decision(models.Model):
+	user = models.ForeignKey(User)
+	position = models.IntegerField()
+
 class Bucket(models.Model):
+	decision = models.ForeignKey(Decision)
 	name = models.CharField(max_length=200)
+	image_url = models.CharField(max_length=200)
+	position = models.IntegerField()
+
+class Item(models.Model):
+	bucket = models.ForeignKey(Bucket)
+	name = models.CharField(max_length=200)
+	position = models.IntegerField()
+
+class Attribute(models.Model):
+	item = models.ForeignKey(Item)
+	name = models.CharField(max_length=200)
+	field = models.CharField(max_length=1000)
+	position = models.IntegerField()
 	
