ecae4c1434293ef55afeecd8c3070a4a84be1dd0
Zack Hsi <zackhsi@gmail.com>
2012-08-04 10:01:29 -0700

    some comment deletion in bucketsTemplate

--- hellodjango/models.py
+++ hellodjango/models.py
@@ -25,7 +25,8 @@ class Item(models.Model):
 	bucket = models.ForeignKey(Bucket)
 	name = models.CharField(max_length=200)
 	position = models.IntegerField()
-	image_url = models.CharField(max_length=1000)
+	image_url = models.TextField()
+# 	image_url = models.CharField(max_length=1000)
 	def __unicode__(self):
 	        return self.bucket.decision.customer.email + "; " + self.bucket.decision.name + "; " + self.bucket.name + "; " + self.name
 
